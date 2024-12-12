714bf36ebaf7b832fc935a013bd692f9e9f4853c
Keonlee Kim <kk545200@nate.com>
2024-12-12 21:31:30 +0900

    Title: 다양하게 출력, Time: 98ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241212/다양하게 출력/print-in-variety.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Total days in Year\n365");
+        System.out.println("Circumference rate\n3.1415926535");
+    }
+}
